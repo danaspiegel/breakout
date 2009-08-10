011d7ab4b664928814f5dbb2c4e780da2b716adc
Dana Spiegel <dana@sociabledesign.com>
2009-08-10 19:41:23 -0400

    Fixed some broken code and added settings file for sociablemarketplaces

---
+++ settings/sociablemarketplaces.py
@@ -0,0 +1,21 @@
+# Production settings
+
+from settings.common import *
+
+DEBUG = False
+DEBUG_SQL = False
+TEMPLATE_DEBUG = DEBUG
+SEND_BROKEN_LINK_EMAILS = True
+
+SECRET_KEY = 'e*xx_n6am*#ets(wc%hr$+ej0ahur$4$997m*v7bo*_112s)k7'
+
+DATABASE_NAME = 'breakout'
+DATABASE_USER = 'breakout'
+DATABASE_PASSWORD = 'breakout'
+DATABASE_HOST = ''
+DATABASE_PORT = ''
+
+# SESSION_COOKIE_SECURE = True
+
+GOOGLE_MAPS_API_KEY = 'ABQIAAAAZ2jpq9DecEp8EYUNWHTHVxRpUKhoLcF3FmAGBjHUc_fLXbvJ_RRJ8m6uNT8nonO-82Y76MQWqs-W8w'
+
