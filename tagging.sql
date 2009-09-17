8428b47677eaf2c18fa018186c7305d52cc4822e
Joe Kondel <joe@joekondel.com>
2009-09-17 15:27:41 -0400

    Added a foreign key check drop to MIGRATION so it imports without error.
    Fixed missing semicolons in tagging.sql

--- tagging.sql
+++ tagging.sql
@@ -3,7 +3,7 @@ CREATE TABLE `tagging_tag` (
   `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
 CREATE TABLE `tagging_taggeditem` (
@@ -18,7 +18,7 @@ CREATE TABLE `tagging_taggeditem` (
   KEY `tagging_taggeditem_object_id` (`object_id`),
   CONSTRAINT `content_type_id_refs_id_e07b113` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`),
   CONSTRAINT `tag_id_refs_id_60aefff3` FOREIGN KEY (`tag_id`) REFERENCES `tagging_tag` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `tagging_synonym` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -28,6 +28,6 @@ CREATE TABLE `tagging_synonym` (
   UNIQUE KEY `name` (`name`),
   KEY `tagging_synonym_tag_id` (`tag_id`),
   CONSTRAINT `tag_id_refs_id_6f121cee` FOREIGN KEY (`tag_id`) REFERENCES `tagging_tag` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 ALTER TABLE `breakout_venue` ADD COLUMN `tags` varchar(255) NOT NULL;
