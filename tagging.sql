4b31f4c8e34fecd2f3ee1ffda8266ff1b90ada5b
Joe Kondel <joe@joekondel.com>
2009-09-17 13:50:26 -0400

    Forgot DDL for the tagging tables.

--- tagging.sql
+++ tagging.sql
@@ -1,1 +1,33 @@
+CREATE TABLE `tagging_tag` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+
+
+CREATE TABLE `tagging_taggeditem` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tag_id` int(11) NOT NULL,
+  `content_type_id` int(11) NOT NULL,
+  `object_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `tag_id` (`tag_id`,`content_type_id`,`object_id`),
+  KEY `tagging_taggeditem_tag_id` (`tag_id`),
+  KEY `tagging_taggeditem_content_type_id` (`content_type_id`),
+  KEY `tagging_taggeditem_object_id` (`object_id`),
+  CONSTRAINT `content_type_id_refs_id_e07b113` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`),
+  CONSTRAINT `tag_id_refs_id_60aefff3` FOREIGN KEY (`tag_id`) REFERENCES `tagging_tag` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+
+CREATE TABLE `tagging_synonym` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `tag_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  KEY `tagging_synonym_tag_id` (`tag_id`),
+  CONSTRAINT `tag_id_refs_id_6f121cee` FOREIGN KEY (`tag_id`) REFERENCES `tagging_tag` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+
 ALTER TABLE `breakout_venue` ADD COLUMN `tags` varchar(255) NOT NULL;
